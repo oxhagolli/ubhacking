e7b22804713b5d4e208a20ad426adb38d29653fd
oxhagolli <orens.xhagolli@hotmail.com>
2015-11-15 03:17:03 -0500

    Did most of home-patient.html; you can view medications from home page

--- UBHacking/pillpal/alphapillpal/models.py
+++ UBHacking/pillpal/alphapillpal/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import json
+
 #will store user's medications. can have more than one
 class Medication(models.Model):
     patient = models.ForeignKey(User)
     name = models.CharField(max_length=30)
-    time = models.TimeField()
+    time = models.TextField(null=True) #JSON-serialized. values of times of the day (list of timefields)
     days = models.TextField(null=True) #JSON-serialized (text) version of list; will hold days of week
+
+    def get_times(self):
+        jsonDec = json.JSONDecoder()
+        lst = jsonDec.decode(self.time)
+
+        str1 = ""
+        for index, item in enumerate(lst):
+            str1 += str(item)
+            if item != len(lst)-1:
+                str1 += ", "
+        return str1
+
+    def get_days(self):
+        jsonDec = json.JSONDecoder()
+        lst = jsonDec.decode(self.days)
+
+        str1 = ""
+        for index, item in enumerate(lst):
+            str1 += str(item)
+            if item != len(lst)-1:
+                str1 += ", "
+        return str1
